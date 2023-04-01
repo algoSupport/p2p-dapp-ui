36cc3590fb3b484517200ff66f0ee617662c5c2a
algoSupport <hollydev0430@gmail.com>
2023-04-01 19:57:34 +0200

    init

--- src/_metronic/layout/components/toolbar/toolbars/ToolbarClassic.tsx
+++ src/_metronic/layout/components/toolbar/toolbars/ToolbarClassic.tsx
@@ -16,12 +16,7 @@ const ToolbarClassic = () => {
     <div className='d-flex align-items-center gap-2 gap-lg-3'>
       {config.app?.toolbar?.filterButton && (
         <div className='m-0'>
-          <a
-            href='#'
-            className={clsx('btn btn-sm btn-flex fw-bold disabled', daterangepickerButtonClass)}
-            data-kt-menu-trigger='click'
-            data-kt-menu-placement='bottom-end'
-          >
+          <a href='#' data-kt-menu-trigger='click' data-kt-menu-placement='bottom-end'>
             <KTSVG
               path='/media/icons/duotune/general/gen031.svg'
               className='svg-icon-6 svg-icon-muted me-1'
@@ -55,7 +50,6 @@ const ToolbarClassic = () => {
       {config.app?.toolbar?.primaryButton && (
         <a
           href='#'
-          onClick={() => setShowCreateAppModal(true)}
           className='btn btn-sm fw-bold btn-primary disabled'
           data-bs-toggle='modal'
           data-bs-target='#kt_modal_create_app'
