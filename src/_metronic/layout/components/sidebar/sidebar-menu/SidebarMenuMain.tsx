93cfa8c931060a74e6bdbbc8309563a6fe3809b3
algoSupport <hollydev0430@gmail.com>
2023-04-02 09:13:01 +0200

    init

--- src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
+++ src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -25,7 +25,7 @@ const SidebarMenuMain = () => {
 
       <div className='menu-item'>
         <div className='menu-content pt-8 pb-2'>
-          <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Account</span>
+          <span className='menu-section text-muted text-uppercase fs-8 ls-1'>My Account</span>
         </div>
       </div>
       <SidebarMenuItem
