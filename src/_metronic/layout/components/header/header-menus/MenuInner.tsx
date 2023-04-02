c9221a7b9522c9a6f546056f8f510ce690854e74
algoSupport <hollydev0430@gmail.com>
2023-04-02 12:26:50 +0200

    init

--- src/_metronic/layout/components/header/header-menus/MenuInner.tsx
+++ src/_metronic/layout/components/header/header-menus/MenuInner.tsx
@@ -1,8 +1,6 @@
-import {useIntl} from 'react-intl'
 import {MenuItem} from './MenuItem'
 
 export function MenuInner() {
-  const intl = useIntl()
   return (
     <>
       <MenuItem title='Marketplace' to='/marketplace' />
