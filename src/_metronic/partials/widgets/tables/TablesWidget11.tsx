5152f5fd61ea381557a95e5b15c038cecba63e72
algoSupport <hollydev0430@gmail.com>
2023-04-04 08:14:36 +0200

    init

--- src/_metronic/partials/widgets/tables/TablesWidget11.tsx
+++ src/_metronic/partials/widgets/tables/TablesWidget11.tsx
@@ -1,155 +1,54 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import {KTSVG} from '../../../helpers'
-import {useState} from 'react'
-import {useRef} from 'react'
-import {CreateAppModal} from '../../../partials'
-
+import {KTSVG, toAbsoluteUrl} from '../../../helpers'
 type Props = {
   className: string
 }
 
 const TablesWidget11: React.FC<Props> = ({className}) => {
-  const [showCreateAppModal, setShowCreateAppModal] = useState<boolean>(false)
-  const btnRef = useRef<HTMLButtonElement | null>(null)
-  const onClick = () => {
-    // Disable indicator after 3 seconds
-    btnRef.current?.setAttribute('data-kt-indicator', 'on')
-    setTimeout(() => {
-      // Activate indicator
-      btnRef.current?.removeAttribute('data-kt-indicator')
-    }, 3000)
-  }
   return (
-    <>
-      <div className='modal fade' tabIndex={-1} id='kt_modal_1'>
-        <div className='modal-dialog modal-dialog-centered'>
-          <div className='modal-content'>
-            <div className='modal-header py-3'>
-              <h5 className='modal-title'>Access Cilistia's Testnet</h5>
-              <div
-                className='btn btn-icon btn-sm btn-active-light-primary ms-2'
-                data-bs-dismiss='modal'
-                aria-label='Close'
-              >
-                <KTSVG
-                  path='/media/icons/duotune/arrows/arr061.svg'
-                  className='svg-icon svg-icon-2x'
-                />
-              </div>
-            </div>
-            <div className='modal-body py-8 overlay overlay-block'>
-              <div>
-                <input
-                  type='email'
-                  className='form-control form-control-white required'
-                  placeholder='Enter your wallet address'
-                />
-              </div>
-              <div className='overlay-layer bg-dark bg-opacity-5'>
-                <div className='spinner-border text-primary' role='status'>
-                  <span className='visually-hidden'>Loading...</span>
-                </div>
-              </div>
-            </div>
-            <div className='modal-footer py-2'>
-              <button type='button' className='btn btn-light btn-sm' data-bs-dismiss='modal'>
-                Close
-              </button>
-              <button
-                ref={btnRef}
-                onClick={onClick}
-                type='button'
-                className='btn btn-primary btn-sm disabled'
-                id='kt_button_1'
-              >
-                <span className='indicator-label'>Verify</span>
-                <span className='indicator-progress'>
-                  Please wait...
-                  <span className='spinner-border spinner-border-sm align-middle ms-2'></span>
-                </span>
-              </button>
-            </div>
-          </div>
+    <div className={`card ${className}`}>
+      {/* begin::Header */}
+      <div className='card-header border-0 pt-5'>
+        <h3 className='card-title align-items-start flex-column'>
+          <span className='card-label fw-bold fs-3 mb-1'>Buy Crypto</span>
+        </h3>
+      </div>
+      {/* end::Header */}
+      {/* begin::Body */}
+      <div className='card-body py-3'>
+        {/* begin::Table container */}
+        <div className='table-responsive'>
+          {/* begin::Table */}
+          <table className='table align-middle gs-0 gy-4'>
+            {/* begin::Table head */}
+            <thead>
+              <tr className='fw-bold text-muted bg-light fs-7'>
+                <th className='ps-4 min-w-180px rounded-start'>Seller</th>
+                <th className='min-w-100px'>Assets</th>
+                <th className='min-w-100px'>Limits</th>
+                <th className='min-w-100px'>Methods</th>
+                <th className='min-w-100px'>Volume Traded</th>
+                <th className='min-w-100px'>Rating</th>
+                <th className='min-w-100px text-end rounded-end'></th>
+              </tr>
+            </thead>
+            {/* end::Table head */}
+            {/* begin::Table body */}
+            <tbody>
+              <tr>
+                <td colSpan={5} className='text-muted'>
+                  Please connect your wallet to view available offers.
+                </td>
+              </tr>
+            </tbody>
+            {/* end::Table body */}
+          </table>
+          {/* end::Table */}
         </div>
       </div>
-      <div className='form mw-350px mx-auto mt-20 pt-20'>
-        {/* begin::Heading */}
-
-        <div className='text-center mb-11'>
-          <h1 className='text-dark fw-bolder mb-3'>Wallet Connect</h1>
-          <div className='text-gray-500 fw-semibold fs-6'>
-            Connect Your Wallet To Access Cilistia
-          </div>
-        </div>
-        {/* begin::Heading */}
-
-        {/* begin::Login options */}
-        <div className='row g-3 mb-9'>
-          {/* begin::Col */}
-          <div className='col-md-6'>
-            {/* begin::Google link */}
-            <a
-              href='#'
-              className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100 disabled'
-            >
-              <img
-                alt='Logo'
-                src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1024px-MetaMask_Fox.svg.png?20220831120339'
-                className='h-20px me-3'
-              />
-              Metamask
-            </a>
-            {/* end::Google link */}
-          </div>
-          {/* end::Col */}
-
-          {/* begin::Col */}
-          <div className='col-md-6'>
-            {/* begin::Google link */}
-            <a
-              href='#'
-              className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100 disabled'
-            >
-              <img
-                alt='Logo'
-                src='https://trustwallet.com/assets/images/media/assets/trust_platform.svg'
-                className='h-20px me-3'
-              />
-              Trust Wallet
-            </a>
-            {/* end::Google link */}
-          </div>
-          {/* end::Col */}
-        </div>
-        {/* end::Login options */}
-
-        {/* begin::Separator */}
-        <div className='separator separator-content my-14'>
-          <span className='w-150px text-gray-500 fw-semibold fs-7'>Can't Connect?</span>
-        </div>
-        {/* end::Separator */}
-
-        {/* end::Wrapper */}
-
-        {/* begin::Action */}
-        <div className='d-grid mb-10'>
-          <a
-            href='#'
-            onClick={() => setShowCreateAppModal(true)}
-            className='btn fw-bold btn-primary'
-            data-bs-toggle='modal'
-            data-bs-target='#kt_modal_create_app'
-          >
-            Verify Wallet
-          </a>
-        </div>
-        <CreateAppModal
-          show={showCreateAppModal}
-          handleClose={() => setShowCreateAppModal(false)}
-        />
-      </div>
-    </>
+      {/* begin::Body */}
+    </div>
   )
 }
 
--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -2,11 +2,31 @@
 import {FC} from 'react'
 import {useIntl} from 'react-intl'
 import {PageTitle} from '../../../_metronic/layout/core'
-import {WalletConnect} from '../../../_metronic/partials/widgets/_new/wallet/WalletConnect'
+import {TablesWidget11} from '../../../_metronic/partials/widgets'
+import {TablesWidget12} from '../../../_metronic/partials/widgets'
 
 const DashboardPage: FC = () => (
   <>
-    <WalletConnect className='mb-5 mb-xl-8' />
+    <ul className='nav nav-tabs nav-line-tabs mb-5 fs-6'>
+      <li className='nav-item'>
+        <a className='nav-link active' data-bs-toggle='tab' href='#kt_tab_pane_1'>
+          Buy
+        </a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' data-bs-toggle='tab' href='#kt_tab_pane_2'>
+          Sell
+        </a>
+      </li>
+    </ul>
+    <div className='tab-content' id='myTabContent'>
+      <div className='tab-pane fade active show' id='kt_tab_pane_1' role='tabpanel'>
+        <TablesWidget11 className='mb-5 mb-xl-8' />
+      </div>
+      <div className='tab-pane fade' id='kt_tab_pane_2' role='tabpanel'>
+        <TablesWidget12 className='mb-5 mb-xl-8' />
+      </div>
+    </div>
   </>
 )
 
